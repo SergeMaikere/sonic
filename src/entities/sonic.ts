5d6a4f9ca9e72391f4e91f6f570c970420334490
SergeMaikere <serge.maikere@gmail.com>
2025-08-14 17:01:04 +0200

    Ring + ring collision

--- src/entities/sonic.ts
+++ src/entities/sonic.ts
@@ -1,15 +1,15 @@
 import K from '../kaplayCtx'
-import type { GameObj, Vec2 } from 'kaplay'
+import type { Vec2 } from 'kaplay'
 
 export const makeSonic = ( pos: Vec2 ) => {
 	return K.add( 
 		[
 			K.sprite('sonic', {anim: 'run'}),
 			K.scale(4),
-			K.area(),
-			K.anchor('center'),
 			K.pos(pos),
-			K.body( {jumpForce: 1700} ),
+			K.anchor('center'),
+			K.area(),
+			K.body( {jumpForce: 1400} ),
 			{
 				setControls () { K.onButtonPress('jump', () => jump(this)) }, 
 				setEvent () { (this as any).onGround(() => (this as any).play('run')) }
--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -1,6 +1,7 @@
 import K from '../kaplayCtx'
 import { makeMotoBug } from '../entities/motobug'
 import { makeSonic } from '../entities/sonic'
+import { makeRing } from '../entities/ring'
 import { infiniteTraveling, setBgGameObject, setPlatformGameObject } from './mainMenu'
 import type { GameObj } from 'kaplay'
 
@@ -10,38 +11,46 @@ export const game = () => {
 	let gameSpeed = -300
 	K.loop( 1, () => gameSpeed -= 50 )
 
-	const bg = setBgGameObject()
-	const plat = setPlatformGameObject()
-	K.onUpdate(
-		() => {
-			infiniteTraveling(bg, -100)
-			infiniteTraveling(plat, gameSpeed)
-		}
-	)
-
-	setSolidPlatform()
-	setSonic()
-	const spawnMotobug = () => {
-		const motobug = makeMotoBug(K.vec2(1950, 775))
-		motobug.onUpdate(
+	const setInfiniteTraveling = () => {
+		const bg = setBgGameObject()
+		const plat = setPlatformGameObject()
+		K.onUpdate(
 			() => {
-				if ( gameSpeed > -2000 ) {
-					motobug.move(gameSpeed - 300, 0)
-					return
-				}
-
-				if ( gameSpeed < -2000 ) {
-					motobug.move( gameSpeed, 0)
-				}
+				infiniteTraveling(bg, -100)
+				infiniteTraveling(plat, gameSpeed)
 			}
 		)
-
-		motobug.onExitScreen( () => motobug.pos.x < 0 && K.destroy(motobug) )
-
-		const waitTime = K.rand(0.5, 2.5)
-		K.wait( waitTime, spawnMotobug )
 	}
-	spawnMotobug()
+
+	const spawn = ( makeEntity: Function, pos: {x: number, y: number}, rand: {min: number, max: number} ) => {
+		const mySpawn = () => {
+			const entity = makeEntity(K.vec2(pos.x, pos.y))
+			entity.onUpdate(
+				() => {
+					if ( gameSpeed > -2000 ) {
+						entity.move(gameSpeed - 300, 0)
+						return
+					}
+
+					if ( gameSpeed < -2000 ) {
+						entity.move( gameSpeed, 0)
+					}
+				}
+			)
+
+			entity.onExitScreen( () => entity.pos.x < 0 && K.destroy(entity) )
+			const waitTime = K.rand(rand.min, rand.max)
+			K.wait( waitTime, mySpawn )
+		}
+		mySpawn()
+	}
+
+
+	setInfiniteTraveling()
+	spawn( makeMotoBug, {x: 1950, y: 775}, {min: 0.5, max: 2.5} ) 
+	spawn( makeRing, {x: 1950, y: 765}, {min: 0.5, max: 3} ) 
+	setSolidPlatform()
+	setSonic()
 }
 
 const setSolidPlatform = () => K.add( [K.rect(1920, 300), K.pos(0, 832), K.area(), K.body({isStatic: true}), K.opacity(0)] )
@@ -50,10 +59,11 @@ const setSonic = () => {
 	const sonic =  makeSonic(K.vec2(200, 745))
 	sonic.setControls()
 	sonic.setEvent()
-	sonic.onCollide( 'enemy', (enemy: GameObj) => setCollision(sonic, enemy) )
+	sonic.onCollide( 'enemy', (enemy: GameObj) => setEnemyCollision(sonic, enemy) )
+	sonic.onCollide( 'ring', (ring: GameObj) => setRingCollision(ring) )
 }
 
-const setCollision = ( sonic: GameObj, enemy: GameObj ) => {
+const setEnemyCollision = ( sonic: GameObj, enemy: GameObj ) => {
 	if ( !sonic.isGrounded() ) return rebound(sonic, enemy)
 	gameover()
 }
@@ -69,4 +79,9 @@ const rebound = ( sonic: GameObj, enemy: GameObj ) => {
 const gameover = () => {
 	K.play('Hurt', {volume: 0.5})
 	K.go('gameover')
+}
+
+const setRingCollision = ( ring: GameObj ) => {
+	K.play('Ring', {volume: 0.5})
+	K.destroy(ring)
 }
